2a36faa0e88c753e941601e38af32f0048cecb29
Programador Novato <info@programadornovato.com>
2019-06-05 02:09:55 -0500

    30.-Vue js 2 tutorial español 👀 [vuetifyjs grid]👀👀

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,30 @@
 <template>
-  <HelloWorld />
+  <v-container >
+    <v-layout wrap>
+      <v-flex class="primary" xs6 >
+        6
+      </v-flex>
+      <v-flex class="error" xs6 >
+        6
+      </v-flex>
+      <v-flex class="secondary" xs6 >
+        6
+      </v-flex>
+      <v-flex class="success" xs6 >
+        6
+      </v-flex>
+    </v-layout>
+
+
+    <v-layout wrap justify-center align-center class="mt-5 secondary"  style="height:500px">
+      <v-flex xs3 class="primary" >3</v-flex>
+      <v-flex xs3 class="success" >3</v-flex>
+      <v-flex xs3 class="error" >3</v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
-  import HelloWorld from '../components/HelloWorld'
-
   export default {
-    components: {
-      HelloWorld
-    }
   }
 </script>
