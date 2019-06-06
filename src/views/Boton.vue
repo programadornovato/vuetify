39631927de7b15bdff484057619304dbddd16954
Programador Novato <info@programadornovato.com>
2019-06-06 13:39:24 -0500

    32.-Vue js 2 base

---
+++ src/views/Boton.vue
@@ -0,0 +1,18 @@
+<template>
+    <v-container>
+        <v-layout>
+            <v-flex>
+            </v-flex>
+        </v-layout>
+    </v-container>    
+</template>
+<script>
+export default {
+    data(){
+        return{
+
+        };
+    }
+}
+</script>
+
