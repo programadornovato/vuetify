4f14bacdb527f26b17b33f9c180bd74697efe388
Programador Novato <info@programadornovato.com>
2019-06-06 13:38:57 -0500

    31.-Vue js 2 tutorial español 👀 [vuetifyjs color,alignment,typography,spacing]👀👀
    https://www.youtube.com/edit?o=U&video_id=PQNivv6B3Vg

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -17,9 +17,15 @@
 
 
     <v-layout wrap justify-center align-center class="mt-5 secondary"  style="height:500px">
-      <v-flex xs3 class="primary" >3</v-flex>
-      <v-flex xs3 class="success" >3</v-flex>
+      <v-flex xs3 class="primary deep-purple darken-2 white--text" >3</v-flex>
+      <v-flex xs3 class="success display-3 text-uppercase" >3 texto</v-flex>
       <v-flex xs3 class="error" >3</v-flex>
+    </v-layout>
+    <v-layout>
+      <v-flex xs3 class="" >texto 1</v-flex>
+      <v-flex xs3 class="" >texto 2</v-flex>
+      <v-flex xs3 class="primary text-xs-center" >texto 3</v-flex>
+      <v-flex xs3 class="" >texto 4</v-flex>
     </v-layout>
   </v-container>
 </template>
