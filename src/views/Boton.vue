362b273240d343ffdecd9354c326b292a6253288
Programador Novato <info@programadornovato.com>
2019-06-06 15:14:09 -0500

    32.-Vue js 2 tutorial español 👀 [vuetifyjs botones]👀👀
    https://youtu.be/QE4mPJ7QGIY

--- src/views/Boton.vue
+++ src/views/Boton.vue
@@ -2,6 +2,16 @@
     <v-container>
         <v-layout>
             <v-flex>
+                <v-btn color="success">boton</v-btn>
+                <v-btn dark color="blue">boton</v-btn>
+                <v-btn flat color="blue">boton</v-btn>
+                <v-btn small color="blue">boton</v-btn>
+                <v-btn color="blue">boton</v-btn>
+                <v-btn large color="blue">boton</v-btn>
+                <v-btn dark depressed color="blue">boton</v-btn>
+                <v-btn  flat icon color="success"><v-icon>account_balance</v-icon></v-btn>
+                <v-btn :large="largo" color="success"><v-icon>account_balance</v-icon></v-btn>
+                <v-btn @click="largo^=true;" >click</v-btn>
             </v-flex>
         </v-layout>
     </v-container>    
@@ -10,7 +20,7 @@
 export default {
     data(){
         return{
-
+            largo:true
         };
     }
 }
