19d50104fb6b7eeb159ed8b4897edeb9c95a2517
Programador Novato <info@programadornovato.com>
2019-06-07 16:48:51 -0500

    35.-Vue js 2 tutorial español 👀 [vuetifyjs dialogs,persistent]👀👀
    https://youtu.be/iCb0lYrLjI4

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,7 +8,7 @@
           <v-card-title>Lorem, ipsum dolor.</v-card-title>
           <v-card-text>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Provident eaque quos ad consequatur minus recusandae accusamus facere suscipit delectus, numquam, porro tenetur temporibus repudiandae vero rem voluptatem earum error excepturi?</v-card-text>
           <v-card-actions>
-            <v-btn flat>Accion</v-btn>
+            <v-btn flat @click="dialog=true;">Accion</v-btn>
           </v-card-actions>
         </v-card>
       </v-flex>
@@ -19,7 +19,7 @@
           <v-card-title>Lorem, ipsum dolor.</v-card-title>
           <v-card-text>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Provident eaque quos ad consequatur minus recusandae accusamus facere suscipit delectus, numquam, porro tenetur temporibus repudiandae vero rem voluptatem earum error excepturi?</v-card-text>
           <v-card-actions>
-            <v-btn flat>Accion</v-btn>
+            <v-btn flat @click="dialog=true;">Accion</v-btn>
           </v-card-actions>
         </v-card>
       </v-flex>
@@ -30,7 +30,7 @@
           <v-card-title>Lorem, ipsum dolor.</v-card-title>
           <v-card-text>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Provident eaque quos ad consequatur minus recusandae accusamus facere suscipit delectus, numquam, porro tenetur temporibus repudiandae vero rem voluptatem earum error excepturi?</v-card-text>
           <v-card-actions>
-            <v-btn flat>Accion</v-btn>
+            <v-btn flat @click="dialog=true;">Accion</v-btn>
           </v-card-actions>
         </v-card>
       </v-flex>
@@ -41,15 +41,29 @@
           <v-card-title>Lorem, ipsum dolor.</v-card-title>
           <v-card-text>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Provident eaque quos ad consequatur minus recusandae accusamus facere suscipit delectus, numquam, porro tenetur temporibus repudiandae vero rem voluptatem earum error excepturi?</v-card-text>
           <v-card-actions>
-            <v-btn flat>Accion</v-btn>
+            <v-btn flat @click="dialog=true;">Accion</v-btn>
           </v-card-actions>
         </v-card>
       </v-flex>
     </v-layout>
+    <v-dialog v-model="dialog" width="500" persistent >
+        <v-card>
+          <v-card-title>Lorem, ipsum dolor.</v-card-title>
+          <v-card-text>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Provident eaque quos ad consequatur minus recusandae accusamus facere suscipit delectus, numquam, porro tenetur temporibus repudiandae vero rem voluptatem earum error excepturi?</v-card-text>
+          <v-card-actions>
+            <v-btn flat @click="dialog=false;" >Cerrar</v-btn>
+          </v-card-actions>
+        </v-card>
+    </v-dialog>
   </v-container>
 </template>
 
 <script>
   export default {
+    data(){
+      return{
+        dialog:false
+      }
+    }
   }
 </script>
